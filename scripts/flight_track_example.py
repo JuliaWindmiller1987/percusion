a3c6e3d167a5a5f3abcd62998795c3129243431a
Julia Windmiller <julia.windmiller@mpimet.mpg.de>
2026-03-24 09:28:38 +1100

    Adds script and figure to illustrate generic flight pattern.

---
+++ scripts/flight_track_example.py
@@ -0,0 +1,118 @@
+# %%
+
+"""Generate image with flight track superimposed on GOES visible image"""
+
+from orcestra.flightplan import LatLon, path_preview, path_as_ds, plot_path
+from orcestra.weathermaps import goes_overlay
+import xarray as xr
+import numpy as np
+from scipy.stats import binned_statistic_2d
+import h5py
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cartopy.crs as ccrs
+
+sns.set_context("talk")
+
+# %%
+
+lon_min = -62
+lon_max = -10
+lat_min = -2
+lat_max = 22
+
+# %%
+all_tracks = xr.open_dataset(
+    "ipfs://bafybeias3h5uxtt4ky4d4gn6l6gxjqfkzbde5jlunya6g3umnkvn7xoyoe", engine="zarr"
+)
+
+# %%
+
+flight_name = "HALO-20240811a"
+flight_date = flight_name[5:9] + "-" + flight_name[9:11] + "-" + flight_name[11:13]
+
+tracks = all_tracks.sel(time=flight_date)
+plan = LatLon(lat=tracks["lat"], lon=tracks["lon"], label=flight_name)
+
+sat_date_time = flight_date + "T15:20"
+loc_at_sat = tracks.assign_coords({"tid": tracks.time}).sel(
+    time=sat_date_time, method="nearest"
+)
+
+# %%
+# Advanced Microwave Scanning Radiometer (AMSR)
+# Integrated water vapor in atmospheric column (TCWV) in mm
+# https://nsidc.org/data/au_rain/versions/1 (download from https://nsidc.org/data/data-access-tool/AU_Rain/versions/1)
+
+with h5py.File("./data/AMSR_U2_L2_Rain_V02_202408111422_A.he5", "r") as f:
+    swath = f["HDFEOS"]["SWATHS"]["AMSR2_L1R"]
+    data = swath["Data Fields"]
+    geo = swath["Geolocation Fields"]
+
+    lat = geo["Latitude"][:]
+    lon = geo["Longitude"][:]
+    tcwv = data["TotalColWaterVapor"][:]
+
+lon_flat = lon.flatten()
+lat_flat = lat.flatten()
+tcwv_flat = tcwv.flatten()
+
+lon_grid = np.linspace(lon_min, lon_max, 250)
+lat_grid = np.linspace(lat_min, lat_max, 250)
+
+tcwv_grid, lon_edges, lat_edges, _ = binned_statistic_2d(
+    lon_flat, lat_flat, tcwv_flat, statistic="mean", bins=[lon_grid, lat_grid]
+)
+
+# %%
+size = 8
+aspect = 16 / 9
+
+map_extent = [lon_min, lon_max, lat_min, lat_max]
+
+fig, ax = plt.subplots(
+    figsize=(size * aspect, size),
+    subplot_kw={"projection": ccrs.PlateCarree()},
+)
+
+ax.set_extent(map_extent, crs=ccrs.PlateCarree())
+ax.coastlines(alpha=1.0, color="white")
+
+gl = ax.gridlines(
+    draw_labels=True,
+    alpha=0.25,
+    xlocs=range(-60, -10, 10),
+    ylocs=range(0, 25, 5),
+)
+
+gl.top_labels = False
+gl.right_labels = False
+
+plt.plot(plan.lon, plan.lat, "C1")
+# plt.scatter(loc_at_sat.lon, loc_at_sat.lat, color="C1", )
+
+goes_overlay(sat_date_time, ax)
+
+# Satellite swath
+
+plt.contourf(
+    lon_grid[:-1] + np.diff(lon_grid) / 2,
+    lat_grid[:-1] + np.diff(lat_grid) / 2,
+    tcwv_grid.T,
+    extent=map_extent,
+    cmap="Blues",
+    alpha=0.75,
+    levels=np.arange(36, 68, 4),
+)
+
+plt.colorbar(
+    label="CWV / mm",
+    ax=ax,
+    orientation="horizontal",
+    pad=0.1,
+    shrink=0.5,
+)
+
+plt.savefig("./figures/generic_flight_track.pdf", bbox_inches="tight")
+# %%
