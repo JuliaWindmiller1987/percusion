16dfb50c375864366c9252545b0ba2eb0badd4c5
Julia Windmiller <julia.windmiller@mpimet.mpg.de>
2026-04-10 18:26:02 +1000

    First draft of surface flux calculation (incomplete!)

--- scripts/figures_mse_radiation.py
+++ scripts/figures_mse_radiation.py
@@ -68,3 +68,69 @@ plt.ylim(ymin=0)
 sns.despine()
 
 plt.savefig(f"{PROJECT_ROOT}/figures/mse_radiation.pdf", bbox_inches="tight")
+# %%
+
+ds_meteor_ec_underpass = ds_meteor.sel(time=slice(ec_start, ec_end))
+
+num_dship_ts = len(ds_meteor_ec_underpass["time"])
+
+sst = np.empty((num_dship_ts, 2))
+sst[:, 0] = ds_meteor_ec_underpass["sst_extern_board"].values
+sst[:, 1] = ds_meteor_ec_underpass["sst_extern_port"].values
+sst_mean = np.mean(sst, axis=1)
+
+t_air = np.empty((num_dship_ts, 2))
+t_air[:, 0] = ds_meteor_ec_underpass["t_air_board"].values
+t_air[:, 1] = ds_meteor_ec_underpass["t_air_port"].values
+t_air_mean = np.mean(t_air, axis=1)
+
+wspd = ds_meteor_ec_underpass["wspd"].values
+
+# Need to check this!!!
+
+rho = 1.2  # kg/m^3
+cp = 1004  # J/kg/K
+CH = 1.2e-3  # typical neutral value
+
+sensible_heat_flux = rho * cp * CH * wspd * (sst_mean - t_air_mean)  # W/m^2
+# %%
+
+rh = np.empty((num_dship_ts, 2))
+rh[:, 0] = ds_meteor_ec_underpass["rh_board"].values
+rh[:, 1] = ds_meteor_ec_underpass["rh_port"].values
+rh_mean = np.mean(rh, axis=1)
+
+
+def saturation_vapor_pressure(T):
+    # T in °C, returns hPa
+    return 6.112 * np.exp(17.67 * T / (T + 243.5))
+
+
+def specific_humidity(e, p):
+    # e in hPa, p in hPa
+    return 0.622 * e / p
+
+
+def latent_heat_flux(Ts, Ta, RH, U, p=1013):
+    # Ts, Ta in °C; RH in [0,1]; U in m/s
+
+    es_Ts = saturation_vapor_pressure(Ts)
+    es_Ta = saturation_vapor_pressure(Ta)
+
+    qs = specific_humidity(es_Ts, p)
+    qa = specific_humidity(RH * es_Ta, p)
+
+    rho = 1.2
+    Lv = 2.5e6
+    CE = 1.2e-3
+
+    QE = rho * Lv * CE * U * (qs - qa) / 1000
+
+    print(qs - qa)
+
+    return QE
+
+
+latent_heat_flux_values = latent_heat_flux(sst_mean, t_air_mean, rh_mean / 100, wspd)
+latent_heat_flux_values
+# %%
